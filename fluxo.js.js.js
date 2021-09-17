75cfc0121464b20c44679c553904ccf552220cc3
wilton0321 <90014955+wilton0321@users.noreply.github.com>
2021-09-16 22:27:20 -0300

    Add files via upload

---
+++ fluxo.js.js.js
@@ -0,0 +1,6 @@
+var  limite  =  20
+for ( var  valor  =  0 ;  valor  <= limite ;  valor ++  ){
+    if  ( valor  ===  0 )  {  console . log ( "Zero" )  } 
+    else  if  ( valor  %  2  == 0 )  {  console . log ( valor  +  ": é Par" )  }
+    else  {  console . log ( valor  +  ": é Impar"  )  }
+    }
